483aaf46aed4f644460ae4d50d1684e3bebcb809
martinbalsam <giuliorasi@gmail.com>
2015-07-02 18:25:38 +0200

    now the k-means algorithm works for 1-dimentional data

--- k_means_algorithm.py
+++ k_means_algorithm.py
@@ -17,7 +17,9 @@ def initial(k,data):
     for i in sampl:
             m.append(data[i])
     return m           
-
+                                                
+    
+    
 "m are considered as the means of the k clusters" 
 
 
@@ -36,32 +38,22 @@ def assignment(data, means):
     
     
     
-"input: data set and the number k of clusters that we want
-"output: clusters_new is a list of lists, each one is a cluster
-def k_means_clustering(data,k):
-        means=initial(k,data)
-        clusters_old=[]
-        clusters_new=assignment(data,means)
-        while not (clusters_old==clusters_new):
-            for i in range(len(means)):
-                means[i]=np.mean(clusters_new[i])
-            old_means=means
-            clusters_old=clusters_new
-            clusters_new=assignment(data,old_means)
-        return clusters_new
-
-
-
+"input: data set and the number k of clusters that we want"
+"output: clusters_new is a list of lists, each one is a cluster"
 def k_means_clustering_faster(data,k):
-        new_means=initial(k,data)
-        old_means=zeros_as(new_means)
-        while np.norm(new_means-old_means, np.inf)>1.0E-13: #Define max_norm or find something similar in numpy
+        new_means=np.array(initial(k,data))
+        old_means=np.zeros_like(new_means)
+        while not np.allclose(new_means,old_means,rtol=1e-5): #Define max_norm or find something similar in numpy
                 clusters=assignment(data,new_means)
                 old_means=new_means
                 for i in range(len(new_means)):
                         new_means[i]=np.mean(clusters[i])
         return clusters
 
+def time_series_states_vector(data,clusters):
+        ts_states_vector=[]:
+                for x in data:
+                        for i in len(clusters):
 
 def fitness(clusters,centers):
         fitness=0
