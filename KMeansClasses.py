0e920887a457e7c08e2a4425f8b4ee0660b84241
nb93 <feregal@live.de>
2015-07-09 13:47:55 +0200

    Fixed Kmeans Error

--- KMeansClasses.py
+++ KMeansClasses.py
@@ -108,9 +108,9 @@ class Clustering:
         #iterations
         for iterations in range(0,self.maxit):   
             
-            #for clusters in self.clusters:
+            for clusters in self.clusters:
             #    print (clusters.center, len(clusters.points))
-            #    clusters.UpdateMean()
+                clusters.UpdateMean()
             #print "#"  
             
             for cluster in self.clusters:
