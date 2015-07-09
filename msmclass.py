f6ae6604621f240db6e71658146001c2bc15d77b
martinbalsam <giuliorasi@gmail.com>
2015-07-09 17:21:52 +0200

    added pcca, now it works for reversible transition matrices

--- msmclass.py
+++ msmclass.py
@@ -1,131 +1,135 @@
-import numpy as np
-NPA_DIMENSION_ERROR=1
-NPA_SQUARE_ERROR=1
-NPA_TYPE_ERROR=2
-NPA_ENTRIE_ERROR=2
-SYS_CREATE_MSMANALYSIS=100
-SYS_ASSIGN_TRANSITION=101
-class LogFile:
-    def __init__(self,comment):
-        self.comment=comment
-        self.logfile=[]
-    def LogSystemEvent(self, message, objectdesc,code):
-        self.logfile.append((message,objectdesc,str(code)))
-    def LogError(self,errormessage, objectdesc,code):
-        self.logfile.append((errormessage,objectdesc, str(code)))
-        
-TEST_LOG=LogFile("testLog")
-class MSMAnalysis:
-    def __init__(self, T, log=TEST_LOG, lagtime = 1.):
-        """ input:
-        T : transition matrix, row stochastic np.array
-        log : string of log file
-        """
-    
-        #initialize the transition matrix
-        if type(T)==np.ndarray:
-            if (T.ndim==2):
-                if (T.shape[0]==T.shape[1]):
-                    check=1
-                    for j in T:
-                        for i in j:
-                            if ((i<0) or (i>1)):
-                                check=0
-                    if (check==1):
-                        self.T=T
+    import numpy as np
+    from pyemma.msm.analysis import pcca as pyemmapcca
+    NPA_DIMENSION_ERROR=1
+    NPA_SQUARE_ERROR=1
+    NPA_TYPE_ERROR=2
+    NPA_ENTRIE_ERROR=2
+    SYS_CREATE_MSMANALYSIS=100
+    SYS_ASSIGN_TRANSITION=101
+    class LogFile:
+        def __init__(self,comment):
+            self.comment=comment
+            self.logfile=[]
+        def LogSystemEvent(self, message, objectdesc,code):
+            self.logfile.append((message,objectdesc,str(code)))
+        def LogError(self,errormessage, objectdesc,code):
+            self.logfile.append((errormessage,objectdesc, str(code)))
+
+    TEST_LOG=LogFile("testLog")
+    class MSMAnalysis:
+        def __init__(self, T, log=TEST_LOG, lagtime = 1.):
+            """ input:
+            T : transition matrix, row stochastic np.array
+            log : string of log file
+            """
+
+            #initialize the transition matrix
+            if type(T)==np.ndarray:
+                if (T.ndim==2):
+                    if (T.shape[0]==T.shape[1]):
+                        check=1
+                        for j in T:
+                            for i in j:
+                                if ((i<0) or (i>1)):
+                                    check=0
+                        if (check==1):
+                            self.T=T
+                        else:
+                            self.log.LogError("Unvalid transitionMatrix,entries not between 0 and 1 ",str(self),NPA_ENTRIE_ERROR)
+                            self.T=NPA_SQUARE_ERROR
+                            self.active=False
                     else:
-                        self.log.LogError("Unvalid transitionMatrix,entries not between 0 and 1 ",str(self),NPA_ENTRIE_ERROR)
+                        self.log.LogError("Unvalid transitionMatrix, not square but " +str(T.shape),str(self),NPA_DIMENSION_ERROR)
                         self.T=NPA_SQUARE_ERROR
                         self.active=False
                 else:
-                    self.log.LogError("Unvalid transitionMatrix, not square but " +str(T.shape),str(self),NPA_DIMENSION_ERROR)
-                    self.T=NPA_SQUARE_ERROR
+                    print "Unvalid transition matrix, Dimension must equal 2, object deleted"
+                    self.log.LogError("Unvalid transitionMatrix dimenion " +str(T.ndim),str(self),NPA_DIMENSION_ERROR)
+                    self.T=NPA_DIMENSION_ERROR
                     self.active=False
-            else:
-                print "Unvalid transition matrix, Dimension must equal 2, object deleted"
-                self.log.LogError("Unvalid transitionMatrix dimenion " +str(T.ndim),str(self),NPA_DIMENSION_ERROR)
-                self.T=NPA_DIMENSION_ERROR
-                self.active=False
-                
-        else:
-            print "Unvalid transition matrix, must be of type numpy array, object deleted"
-            self.log.LogError("Unvalid transitionMatrix type "+ str(type(T)),str(self),NPA_TYPE_ERROR)
-            self.T=NPA_TYPE_ERROR
-            self.active=False
-            
-        self.active=True
-        
-        self.lagtime = lagtime
-        
-        # eigenvalues eigenvectors and stationary distribution
 
-        unsorted_eigenvalues,unsorted_Reigenvectors= np.linalg.eig(self.T)
-        _, unsorted_Leigenvectors = np.linalg.eig(np.transpose(self.T))
-        self._unsorted_eigenvalues = unsorted_eigenvalues
-        unsorted_Reigenvectors = np.transpose(unsorted_Reigenvectors)
-        unsorted_Leigenvectors = np.transpose(unsorted_Leigenvectors)
-        #zip eigenvalues and eigenvectors
-        Rzipper = zip(unsorted_eigenvalues,unsorted_Reigenvectors)
-        Lzipper = zip(unsorted_eigenvalues,unsorted_Leigenvectors)
+            else:
+                print "Unvalid transition matrix, must be of type numpy array, object deleted"
+                self.log.LogError("Unvalid transitionMatrix type "+ str(type(T)),str(self),NPA_TYPE_ERROR)
+                self.T=NPA_TYPE_ERROR
+                self.active=False
+
+            self.active=True
+
+            self.lagtime = lagtime
+
+            # eigenvalues eigenvectors and stationary distribution
+
+            unsorted_eigenvalues,unsorted_Reigenvectors= np.linalg.eig(self.T)
+            _, unsorted_Leigenvectors = np.linalg.eig(np.transpose(self.T))
+            self._unsorted_eigenvalues = unsorted_eigenvalues
+            unsorted_Reigenvectors = np.transpose(unsorted_Reigenvectors)
+            unsorted_Leigenvectors = np.transpose(unsorted_Leigenvectors)
+            #zip eigenvalues and eigenvectors
+            Rzipper = zip(unsorted_eigenvalues,unsorted_Reigenvectors)
+            Lzipper = zip(unsorted_eigenvalues,unsorted_Leigenvectors)
+
+            #sort zippers
+            sorted_Rzipper = sorted(Rzipper, reverse = True, key = lambda x: np.abs(x[0]))
+            sorted_Lzipper = sorted(Lzipper, reverse = True, key = lambda x: np.abs(x[0]))
+
+            #unzip
+            self.eigenvalues, self.Reigenvectors = zip(*sorted_Rzipper)
+            _trash, self.Leigenvectors = zip(*sorted_Lzipper)
+
+            # we want to check first the transition matrix, thus the lazy definition
+            self._stationaryDistribution = None
+
+            self._timescales = None
+
+            self.log=log
+            self.log.LogSystemEvent("New instance of MSMAnalysis created",str(self),SYS_CREATE_MSMANALYSIS)
+            self.log.LogSystemEvent("Trying to assign transition matrix to object, variable type " + str(type(T)), str(self),SYS_ASSIGN_TRANSITION)
+
+
+        # Eigenvalues & left and right eigenvectors 
+
+        @property    
+        def stationaryDistribution(self):
+            if (self.active==True):
+                if self._stationaryDistribution is None:
+                    if np.allclose(self.eigenvalues[0],1):
+                        self._stationaryDistribution = np.abs(self.Leigenvectors[0]/np.sum(self.Leigenvectors[0]))
+                    else:
+                        raise ValueError("the biggest eigenvalue is not 1, something is wrong")
+                return self._stationaryDistribution
+
+        @property
+        def istransitionmatrix(self):
+            for i in range(np.shape(self.T)[0]):
+                if not np.allclose(sum(self.T[i,:]), 1):
+                    return False    
+            return True
+
+
+        @property
+        def isreversible(self):
+            """
+            checks the reversed balance condition p_i T_ij == p_j T_ij
+            """
+            P=diag(self.stationaryDistribution)
+            return np.allclose(np.dot(P,self.T),np.dot(np.transpose(self.T),P))  #T.T is T transposed, I know...
+
+        @property
+        def timescales(self,num_of_val = 7):
+            if self._timescales is None:
+                real_val = np.real(self.eigenvalues[1:num_of_val]) # exclude the first eigenvalue 1
+
+                self._timescales = -self.lagtime /np.log(np.absolute(real_val))
+                for i in range(len(self._timescales)):
+                    if self._timescales[i]<0:
+                        self._timescales[i] = np.inf
+
+            return self._timescales
         
-        #sort zippers
-        sorted_Rzipper = sorted(Rzipper, reverse = True, key = lambda x: np.abs(x[0]))
-        sorted_Lzipper = sorted(Lzipper, reverse = True, key = lambda x: np.abs(x[0]))
+        def pcca(self,m=2):
+            return pyemmapcca(self.T,m)
         
-        #unzip
-        self.eigenvalues, self.Reigenvectors = zip(*sorted_Rzipper)
-        _trash, self.Leigenvectors = zip(*sorted_Lzipper)
-        
-        # we want to check first the transition matrix, thus the lazy definition
-        self._stationaryDistribution = None
-        
-        self._timescales = None
-        
-        self.log=log
-        self.log.LogSystemEvent("New instance of MSMAnalysis created",str(self),SYS_CREATE_MSMANALYSIS)
-        self.log.LogSystemEvent("Trying to assign transition matrix to object, variable type " + str(type(T)), str(self),SYS_ASSIGN_TRANSITION)
-    
-    
-    # Eigenvalues & left and right eigenvectors 
-            
-    @property    
-    def stationaryDistribution(self):
-        if (self.active==True):
-            if self._stationaryDistribution is None:
-                if np.allclose(self.eigenvalues[0],1):
-                    self._stationaryDistribution = np.abs(self.Leigenvectors[0]/np.sum(self.Leigenvectors[0]))
-                else:
-                    raise ValueError("the biggest eigenvalue is not 1, something is wrong")
-            return self._stationaryDistribution
-        
-    @property
-    def istransitionmatrix(self):
-        for i in range(np.shape(self.T)[0]):
-            if not np.allclose(sum(self.T[i,:]), 1):
-                return False    
-        return True
-    
-    
-    @property
-    def isreversible(self):
-        """
-        checks the reversed balance condition p_i T_ij == p_j T_ij
-        """
-        P=diag(self.stationaryDistribution)
-        return np.allclose(np.dot(P,self.T),np.dot(np.transpose(self.T,P))  #T.T is T transposed, I know...
-    
-    @property
-    def timescales(self,num_of_val = 7):
-        if self._timescales is None:
-            real_val = np.real(self.eigenvalues[1:num_of_val]) # exclude the first eigenvalue 1
-            
-            self._timescales = -self.lagtime /np.log(np.absolute(real_val))
-            for i in range(len(self._timescales)):
-                if self._timescales[i]<0:
-                    self._timescales[i] = np.inf
-                    
-        return self._timescales
-    
-## TODO: order the eigenvalues in decreasing absolute value
-    def check_stat_dist(self):
-        return np.linalg.norm(self.stationaryDistribution - np.dot(self.stationaryDistribution,self.T))
+    ## TODO: order the eigenvalues in decreasing absolute value
+        def check_stat_dist(self):
+            return np.linalg.norm(self.stationaryDistribution - np.dot(self.stationaryDistribution,self.T))
