6c2e565cdb0ff55fa29787e88dd7fcbfafd86366
martinbalsam <giuliorasi@gmail.com>
2015-07-10 00:07:40 +0200

    added VisualizeCenters()

--- lib/visual.py
+++ lib/visual.py
@@ -22,3 +22,48 @@
 #def plot_free_energy(clustering,msm):
 #    free_energy = - np.log(msm.stationaryDistribution)
 #    plot(free_energy)
+import matplotlib.pyplot as plt
+import numpy as np
+
+def VisualizeCusters(cl):
+    if cl.system_dimension <=0:
+        raise ValueError("system dimention error")
+    if cl.system_dimension == 1:
+        for cluster in cl.clusters:
+            pts =[]
+            min = -100
+            max = 100
+            for point in cluster.points:
+                pts.append(point.coords[0][0])
+            min = amin(pts)
+            
+            plt.plot(pts,np.zeros_like(pts),'o')
+    if cl.system_dimension >=2:
+        pippo = "do stuff"
+        
+def VisualizeCusters_loc(cl):
+    if cl.system_dimension <=0:
+        raise ValueError("system dimention error")
+    if cl.system_dimension == 1:
+        for cluster in cl.clusters:
+            pts =[]
+            min = -100
+            max = 100
+            for point in cluster.points:
+                pts.append(point.coords[0][0])
+
+            plt.plot(pts,np.zeros_like(pts),'o')
+    if cl.system_dimension >=2:
+        pippo = "do stuff"
+        
+def VisualizeCenters(cl,x=0,y=1):
+    if cl.system_dimension <=0:
+        raise ValueError("system dimention error")
+    if cl.system_dimension == 1:
+        for cluster in cl.clusters:
+            plt.plot(cluster.center[0],cluster.ID,'o')
+    if cl.system_dimension >=2:
+        if (x not in range(cl.system_dimension)) or (y not in range(cl.system_dimension)):
+            raise ValueError("coordinates for projection are outside range")        
+        for cluster in cl.clusters:
+            plt.plot(cluster.center[x],cluster.center[y],'o')
