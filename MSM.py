50482d377bf6536507632f4e16ceebf478aa0d69
martinbalsam <giuliorasi@gmail.com>
2015-07-10 03:25:01 +0200

    Delete MSM.py

--- MSM.py
+++
@@ -1,9 +0,0 @@
-class MSM:
-   def __init__(self,T):
-      self.T=T
-      self._pi = None
-   @property
-   def pi(self):
-      if self._pi = None:
-         self._pi = get_pi(self.T)
-      return self._pi
